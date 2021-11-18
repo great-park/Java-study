dfa11a83fdb1c5b16c354c8a2c19811ccea736e3
great-park <cksgh1735@naver.com>
2021-11-19 02:06:32 +0900

    frist commit

---
+++ BankAccount/src/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+    int balance;
+    Person owner;
+
+    boolean dsposit(int amount){
+
+    }
+
+    boolean withdraw(int amount){
+
+    }
+
+    boolean transfer(Person to, int amount){
+
+    }
+}
