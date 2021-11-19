d2a2d056b384101d77bb6e534a292a973494cb65
great-park <cksgh1735@naver.com>
2021-11-19 21:22:08 +0900

    객체 사용 문제 완료

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -2,15 +2,37 @@ public class BankAccount {
     int balance;
     Person owner;
 
-    boolean dsposit(int amount){
+    boolean deposit(int amount) {
+        if (amount < 0 || owner.cashAmount < amount) {
+            System.out.println("입금 실패입니다. 잔고: " +
+                    balance + "원, " + "현금: " + owner.cashAmount +"원");
+            System.out.println("false");
+            return false;
+        } else {
+            owner.cashAmount = owner.cashAmount - amount;
+            balance = balance + amount;
+            System.out.println(amount + "원 입금하였습니다. "
+                    + "잔고: " + balance + "원, " + "현금: " + owner.cashAmount + "원");
+            System.out.println("true");
+            return true;
+        }
 
     }
 
-    boolean withdraw(int amount){
-
+    boolean withdraw(int amount) {
+        if (amount < 0 || amount > balance) {
+            System.out.println("출금 실패입니다. 잔고: " + balance + "원, "
+                    + "현금: " + owner.cashAmount + "원");
+            System.out.println("false");
+            return false;
+        } else {
+            balance = balance - amount;
+            owner.cashAmount = owner.cashAmount + amount;
+            System.out.println(amount + "원 출금하였습니다. 잔고: "
+                    + balance + "원, " + "현금: " + owner.cashAmount + "원");
+            System.out.println("true");
+            return true;
+        }
     }
 
-    boolean transfer(Person to, int amount){
-
-    }
 }
