ff88112b5f769e441e9de3344aa3773c7a37be3e
great-park <cksgh1735@naver.com>
2021-11-20 23:51:03 +0900

    객체지향 연습

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -2,27 +2,30 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.name = "Kim";
-        p1.age = 28;
+        p1.setAge(28);
         p1.cashAmount = 30000;
 
+        System.out.println(p1.getAge());
 
         BankAccount a1 = new BankAccount();
         a1.balance = 100000;
         p1.account = a1;
         a1.owner = p1;
-
-
+        
         Person p2 = new Person();
         p2.name = "Lee";
-        p2.age = 25;
+        p2.setAge(25);
         p2.cashAmount = 100000;
 
+        System.out.println(p2.getAge());
 
         BankAccount a2 = new BankAccount();
         p2.account = a2;
-
         a2.owner = p2;
         a2.balance = 500000;
+
+
+
         a2.deposit(30000);
         a2.withdraw(170000);
         a2.deposit(620000);
--- BankAccount/src/Person.java
+++ BankAccount/src/Person.java
@@ -1,6 +1,39 @@
 public class Person {
-    String name;
-    int age;
-    int cashAmount;
-    BankAccount account;
+    //접근 제어자 private-해당 class에서만, public-모든 class에 공개
+    private String name;
+    private int age;
+    priate int cashAmount;
+    private BankAccount account;
+
+    public void setAge(int newAge){
+        if(newAge>=0){
+            age = newAge;
+        }
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setCashAmount(int newCashAmount){
+        if(newCashAmount>=0){
+            cashAmount = newCashAmount;
+        }
+    }
+    public int getCashAmount(){
+        return cashAmount;
+    }
+    public void setBankAccount(int newBankAccount){
+        if(newBankAccount>=0){
+            BankAccount = newBankAccount;
+        }
+    }
+    public int getBankAccount(){
+        return BankAccount;
+    }
+
 }
