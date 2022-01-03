1673b12e415bd2dbbc59a2ce6b29408a3e829036
great-park <cksgh1735@naver.com>
2022-01-03 21:52:34 +0900

    edit code

--- General/src/general.java
+++ General/src/general.java
@@ -2,16 +2,13 @@ import java.util.ArrayList;
 
 public class general {
     public static int getSurvivingIndex(int n, int k) {
-        // 솔저 생성
         ArrayList<Integer> soldiers = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             soldiers.add(i);
         }
 
-        // 시체 보관소
         ArrayList<Integer> corpse_list = new ArrayList<>();
 
-        // 최후의 한명이 남는 순간까지 게임을 반복한다
         while (n >= 2) {
             for (int i = 1; i <= k - 1; i++) {
                 soldiers.add(soldiers.get(0));
@@ -22,16 +19,14 @@ public class general {
             n--;
         }
 
-        // 시체 나열
         for (Integer corpse : corpse_list) {
-            System.out.println(corpse + "번 군사가 죽습니다. ");
+            System.out.println(corpse + "out");
         }
 
-        // 최후의 1인
         return soldiers.get(0);
     }
 
     public static void main(String[] args) {
-        System.out.println("김신은 " + getSurvivingIndex(20, 5) + "번 자리에 서있으면 됩니다.");
+        System.out.println("you : " + getSurvivingIndex(20, 5) );
     }
 }
